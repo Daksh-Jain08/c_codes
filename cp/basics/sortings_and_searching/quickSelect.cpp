ce45704ec673966574487efc5a916adc5e2d9073
Daksh <jain.daksh0808@gmail.com>
2024-08-05 01:02:50 +0530

    added the comments in quickSelect and smallestString

--- cp/basics/sortings_and_searching/quickSelect.cpp
+++ cp/basics/sortings_and_searching/quickSelect.cpp
@@ -3,6 +3,20 @@
 
 using namespace std;
 
+// Problem: Given an array of distinct integers and an index k, find the kth
+// smallest element in the array.
+
+// Brute Force approach: Sort the array and return the element at the kth
+// position. This approach take average time of O(NlogN)
+
+// Optimal approach: Use something like a quicksort algorithm because in
+// quicksort, a selected element is put into its correct position in the sorted
+// array. We can start putting element in their correct index and partitioning
+// the array. If the array is {0, 5, 2, 3, 1} and k=3, then we first put 1 at
+// its correct position. After this, the array becomes {0,1,2,3,5}, the correct
+// index of 3 is 3, therefore 3 is the answer, it happened so that the array has
+// been sorted in this process it is not necessary always.
+
 int partition(vector<int> &arr, int start, int end) {
   int pivot = arr[end];
   int i = start - 1;
@@ -20,12 +34,20 @@ int quickSelect(vector<int> &arr, int start, int end, int k) {
   if (start == end) {
     return arr[start];
   }
+  // put the last element in its correct index and partition the array.
   int pivot = partition(arr, start, end);
+  // If the returned index = k, return arr[index]
   if (pivot == k - 1) {
     return arr[pivot];
-  } else if (pivot < k - 1) {
+  }
+  // If the returned index < k - 1, this means the desired element is in the
+  // right half of the array.
+  else if (pivot < k - 1) {
     return quickSelect(arr, pivot + 1, end, k);
-  } else {
+  }
+  // If the returned index > k - 1, this means the desired element is in the
+  // left half of the array.
+  else {
     return quickSelect(arr, start, pivot - 1, k);
   }
 }
--- cp/basics/sortings_and_searching/smallestString.cpp
+++ cp/basics/sortings_and_searching/smallestString.cpp
@@ -3,7 +3,14 @@
 #include <vector>
 
 using namespace std;
+// Problem: Given a list of strings v, return the smallest string that can be
+// made by concatenating the strings in v.
 
+// Approach: We can sort the strings in such a way that the smallest string is
+// formed. We can use a custom comparator function to sort the strings. The
+// comparator function will compare two strings a and b. If a + b < b + a, then
+// a is smaller than b. We can sort the strings using this comparator function
+// and then concatenate the strings to get the smallest string.
 bool comparator(string a, string b) { return a + b < b + a; }
 
 string smallestString(vector<string> v) {
