bb446d5923a0d85abb9588af7c6d8a079bcde02d
Daksh <jain.daksh0808@gmail.com>
2024-08-12 04:52:00 +0530

    added comments to the solution

--- cp/basics/recursions/permutations.cpp
+++ cp/basics/recursions/permutations.cpp
@@ -5,7 +5,6 @@ using namespace std;
 // Problem: Given a string s, print all possible permutations of the string.
 
 // Approach: Take out each character from the string and call the function with the remaining string.
-
 void permute(string output, string input) {
   // Base Case
   if (input.length() == 0) {
@@ -15,13 +14,11 @@ void permute(string output, string input) {
 
   for (int i = 0; i < input.length(); i++) {
     char ch = input[i];
-    // Find the remaining string after removing the character at index i
     string remaining;
     for (int j = 0; j < input.length(); j++) {
       if (j != i)
         remaining += input[j];
     }
-    // Call the function recursively with the updated output and remaining string
     permute(output + ch, remaining);
   }
 }
